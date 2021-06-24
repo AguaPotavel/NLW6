19846c757024b0461e76b8f195269e3e79604e0a
AguaPotavel <ruancarlosmeira@gmail.com>
2021-06-24 13:39:55 -0300

    a

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,30 +1,34 @@
 
 import IllustrationImg from '../../assets/images/illustration.svg';
 import GoogleImage from '../../assets/images/google-icon.svg';
+import LetMeAskLogo from '../../assets/images/logo.svg';
+import '../../styles/auth.scss';
+import {Button} from '../../components/Button'
 
 export function Home(){
     return(
-        <div>
+        <div id="page-auth">
             <aside>
                 <img src={IllustrationImg} alt="ilutração" />
-                <strong>Crie salas de Q&anp;A ao-vivo</strong>
+                <strong>Crie salas de Q&A ao-vivo</strong>
                 <p>tire as duvidas da sua audiência</p>
             </aside>
             <main>
-                <div>
-                    <button>
+                <div className="main-content">
+                    <img src={LetMeAskLogo} alt="let me ask logo"/>
+                    <button className="create-room">
                     <img src={GoogleImage} alt="Logo do google" />
                     Crie suas salas com o Google
                     </button>
-                    <div>ou entre em uma sala</div>
+                    <div className="separator">ou entre em uma sala</div>
                     <form action="">
                         <input 
                             type="text" 
                             placeholder="digite o código da sala"
                         />
-                        <button type="submit">
+                        <Button type="submit">
                             Entrar na sala
-                        </button>
+                        </Button>
                     </form>
                 </div>
             </main>
